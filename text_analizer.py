0b269b0e5edcf823acca25b4ec807fdce93962ed
Jacobo J. Pedrosa <jacoboj.pedrosa@Jacobos-Laptop.local>
2025-12-11 12:25:30 +0100

    modificacion en libreria text_analizer

--- text_analizer.py
+++ text_analizer.py
@@ -9,6 +9,7 @@ def word_list(text):
 
 if __name__ == "__main__":
     print("==Test Text Analizer==")
+    # print(word_list("Hola que tal"))
     # print(len(word_list("Hola que tal")))
 
     assert len(word_list("Hola que tal")) == 3, "word_list failed and not returned the required quantity of words..."
